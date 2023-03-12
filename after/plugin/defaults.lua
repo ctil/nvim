27dd23bf6d97465b1ab08f358a73d3fb3bcfc9de
ctil <ctil@users.noreply.github.com>
2023-03-12 09:18:27 -0600

    Add defaults.lua

---
+++ after/plugin/defaults.lua
@@ -0,0 +1,11 @@
+-- Vim options
+vim.o.relativenumber = true
+vim.o.hlsearch = true
+
+
+-- Keymaps
+vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[F]ind [F]iles' })
+vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = '[F]ind [H]elp' })
+vim.keymap.set('n', '<leader>fw', require('telescope.builtin').grep_string, { desc = '[F]ind current [W]ord' })
+vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, { desc = '[F]ind by [G]rep' })
+vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { desc = '[F]ind [D]iagnostics' })
