0e4923748343131b46f7853643808979d6dd2a8f
ctil <ctil@users.noreply.github.com>
2023-03-27 19:32:38 -0600

    Come on baby, and do the split

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -6,7 +6,7 @@ vim.o.incsearch = true
 vim.o.relativenumber = true
 vim.o.shiftwidth = 4
 vim.o.softtabstop = 4
-vim.o.splitright = true
+vim.o.splitright = false
 vim.o.tabstop = 4
 vim.cmd 'colorscheme onedark'
 
