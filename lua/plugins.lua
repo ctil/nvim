4401fc85a499f1bc81203ca8ac2ebf441b8cec27
ctil <ctil@users.noreply.github.com>
2024-03-03 21:29:56 -0700

    Add back signs. Try out mini.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -14,7 +14,8 @@ require('lazy').setup {
   'tpope/vim-sleuth',
 
   'tpope/vim-unimpaired',
-  'tpope/vim-surround',
+  -- Trying mini.surround instead
+  -- 'tpope/vim-surround',
   'christoomey/vim-tmux-navigator',
   'sindrets/diffview.nvim',
 
@@ -94,10 +95,40 @@ require('lazy').setup {
     },
   },
 
+  { -- Collection of various small independent plugins/modules
+    'echasnovski/mini.nvim',
+    config = function()
+      -- Better Around/Inside textobjects
+      --
+      -- Examples:
+      --  - va)  - [V]isually select [A]round [)]paren
+      --  - yinq - [Y]ank [I]nside [N]ext [']quote
+      --  - ci'  - [C]hange [I]nside [']quote
+      require('mini.ai').setup { n_lines = 500 }
+
+      -- Add/delete/replace surroundings (brackets, quotes, etc.)
+      --
+      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+      -- - sd'   - [S]urround [D]elete [']quotes
+      -- - sr)'  - [S]urround [R]eplace [)] [']
+      require('mini.surround').setup()
+
+      -- ... and there is more!
+      --  Check out: https://github.com/echasnovski/mini.nvim
+    end,
+  },
+
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
       on_attach = function(bufnr)
         local gs = package.loaded.gitsigns
 
