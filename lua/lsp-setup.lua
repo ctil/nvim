e3dbd819fa9750f3ca3e923f3538d4fe3f9852f1
ctil <ctil@users.noreply.github.com>
2026-02-01 08:29:32 -0700

    Format lsp-setup and fix gd

--- lua/lsp-setup.lua
+++ lua/lsp-setup.lua
@@ -1,54 +1,54 @@
 -- [[ Configure LSP ]]
 --  This function gets run when an LSP connects to a particular buffer.
 local on_attach = function(_, bufnr)
-  -- NOTE: Remember that lua is a real programming language, and as such it is possible
-  -- to define small helper and utility functions so you don't have to repeat yourself
-  -- many times.
-  --
-  -- In this case, we create a function that lets us more easily define mappings specific
-  -- for LSP related items. It sets the mode, buffer and description for us each time.
-  local nmap = function(keys, func, desc)
-    if desc then
-      desc = 'LSP: ' .. desc
+    -- NOTE: Remember that lua is a real programming language, and as such it is possible
+    -- to define small helper and utility functions so you don't have to repeat yourself
+    -- many times.
+    --
+    -- In this case, we create a function that lets us more easily define mappings specific
+    -- for LSP related items. It sets the mode, buffer and description for us each time.
+    local nmap = function(keys, func, desc)
+        if desc then
+            desc = 'LSP: ' .. desc
+        end
+
+        vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
     end
 
-    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-  end
+    nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+    -- nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+    nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+    nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+    nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+    nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
-  nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-  -- nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-  nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+    -- See `:help K` for why this keymap
+    nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
 
-  -- See `:help K` for why this keymap
-  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+    -- Lesser used LSP functionality
+    nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+    nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+    nmap('<leader>wl', function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, '[W]orkspace [L]ist Folders')
 
-  -- Lesser used LSP functionality
-  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-  nmap('<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, '[W]orkspace [L]ist Folders')
-
-  -- Create a command `:Format` local to the LSP buffer
-  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-    vim.lsp.buf.format {
-      -- filter = function(client)
-      --   -- Skip vue_ls's formatting
-      --   return client.name ~= 'vue_ls'
-      -- end,
-    }
-  end, { desc = 'Format current buffer with LSP' })
+    -- Create a command `:Format` local to the LSP buffer
+    vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+        vim.lsp.buf.format {
+            -- filter = function(client)
+            --   -- Skip vue_ls's formatting
+            --   return client.name ~= 'vue_ls'
+            -- end,
+        }
+    end, { desc = 'Format current buffer with LSP' })
 end
 
 -- document existing key chains
 -- register which-key VISUAL mode
 -- required for visual <leader>hs (hunk stage) to work
 require('which-key').add {
-  { '<leader>', group = 'VISUAL <leader>', mode = 'v' },
-  { '<leader>h', desc = 'Git [H]unk', mode = 'v' },
+    { '<leader>',  group = 'VISUAL <leader>', mode = 'v' },
+    { '<leader>h', desc = 'Git [H]unk',       mode = 'v' },
 }
 
 -- mason-lspconfig requires that these setup functions are called in this order
@@ -57,53 +57,62 @@ require('mason').setup()
 
 -- local vue_language_server_path = vim.fn.expand '$MASON/packages/vue-language-server/node_modules/@vue/language-server'
 -- local typescript_sdk_path = vim.fn.getcwd() .. '/portals/management/node_modules/typescript/lib'
-local vue_language_server_path = vim.fn.stdpath 'data' .. '/mason/packages/vue-language-server/node_modules/@vue/language-server'
+local vue_language_server_path = vim.fn.stdpath 'data' ..
+    '/mason/packages/vue-language-server/node_modules/@vue/language-server'
 
 local vue_plugin = {
-  name = '@vue/typescript-plugin',
-  location = vue_language_server_path,
-  languages = { 'vue' },
-  configNamespace = 'typescript',
+    name = '@vue/typescript-plugin',
+    location = vue_language_server_path,
+    languages = { 'vue' },
+    configNamespace = 'typescript',
+}
+local svelte_plugin = {
+    name = 'typescript-svelte-plugin',
+    location = vim.fn.stdpath 'data' .. '/mason/packages/svelte-language-server/node_modules/typescript-svelte-plugin',
+    languages = { 'svelte' },
 }
 local vtsls_config = {
-  settings = {
-    vtsls = {
-      tsserver = {
-        globalPlugins = {
-          vue_plugin,
+    on_attach = on_attach,
+    settings = {
+        vtsls = {
+            tsserver = {
+                globalPlugins = {
+                    vue_plugin,
+                    svelte_plugin,
+                },
+            },
         },
-      },
     },
-  },
-  filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+    filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
 }
 
 local vue_ls_config = {
-  on_init = function(client)
-    client.handlers['tsserver/request'] = function(_, result, context)
-      local clients = vim.lsp.get_clients { bufnr = context.bufnr, name = 'vtsls' }
-      if #clients == 0 then
-        vim.notify('Could not found `vtsls` lsp client, vue_lsp would not work without it.', vim.log.levels.ERROR)
-        return
-      end
-      local ts_client = clients[1]
+    on_init = function(client)
+        client.handlers['tsserver/request'] = function(_, result, context)
+            local clients = vim.lsp.get_clients { bufnr = context.bufnr, name = 'vtsls' }
+            if #clients == 0 then
+                vim.notify('Could not found `vtsls` lsp client, vue_lsp would not work without it.', vim.log.levels
+                    .ERROR)
+                return
+            end
+            local ts_client = clients[1]
 
-      local param = unpack(result)
-      local id, command, payload = unpack(param)
-      ts_client:exec_cmd({
-        title = 'vue_request_forward', -- You can give title anything as it's used to represent a command in the UI, `:h Client:exec_cmd`
-        command = 'typescript.tsserverRequest',
-        arguments = {
-          command,
-          payload,
-        },
-      }, { bufnr = context.bufnr }, function(_, r)
-        local response_data = { { id, r.body } }
-        ---@diagnostic disable-next-line: param-type-mismatch
-        client:notify('tsserver/response', response_data)
-      end)
-    end
-  end,
+            local param = unpack(result)
+            local id, command, payload = unpack(param)
+            ts_client:exec_cmd({
+                title = 'vue_request_forward', -- You can give title anything as it's used to represent a command in the UI, `:h Client:exec_cmd`
+                command = 'typescript.tsserverRequest',
+                arguments = {
+                    command,
+                    payload,
+                },
+            }, { bufnr = context.bufnr }, function(_, r)
+                local response_data = { { id, r.body } }
+                ---@diagnostic disable-next-line: param-type-mismatch
+                client:notify('tsserver/response', response_data)
+            end)
+        end
+    end,
 }
 -- nvim 0.11 or above
 vim.lsp.config('vtsls', vtsls_config)
@@ -119,54 +128,54 @@ vim.lsp.enable { 'vtsls', 'vue_ls' }
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
-  gopls = {},
-  pyright = {},
-  eslint = {},
-  jsonls = {},
-  svelte = {},
-  emmet_language_server = {
-    filetypes = { 'vue' },
-  },
-  -- vue_ls = {
-  --   init_options = {
-  --     vue = {
-  --       hybridMode = false,
-  --     },
-  --   },
-  -- },
-  rust_analyzer = {
-    ['rust-analyzer'] = {
-      checkOnSave = {
-        command = 'clippy',
-        enable = false,
-        extraArgs = { '--target-dir', './rust-analyzer-target' },
-      },
+    gopls = {},
+    pyright = {},
+    eslint = {},
+    jsonls = {},
+    svelte = {},
+    emmet_language_server = {
+        filetypes = { 'vue' },
     },
-  },
-  -- ts_ls = {
-  --   init_options = {
-  --     plugins = {
-  --       {
-  --         name = '@vue/typescript-plugin',
-  --         location = vue_language_server_path,
-  --         languages = { 'vue', 'typescript' },
-  --       },
-  --     },
-  --     typescript = { tsdk = typescript_sdk_path },
-  --   },
-  --   filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
-  -- },
-  -- html = { filetypes = { 'html', 'twig', 'hbs'} },
+    -- vue_ls = {
+    --   init_options = {
+    --     vue = {
+    --       hybridMode = false,
+    --     },
+    --   },
+    -- },
+    rust_analyzer = {
+        ['rust-analyzer'] = {
+            checkOnSave = {
+                command = 'clippy',
+                enable = false,
+                extraArgs = { '--target-dir', './rust-analyzer-target' },
+            },
+        },
+    },
+    -- ts_ls = {
+    --   init_options = {
+    --     plugins = {
+    --       {
+    --         name = '@vue/typescript-plugin',
+    --         location = vue_language_server_path,
+    --         languages = { 'vue', 'typescript' },
+    --       },
+    --     },
+    --     typescript = { tsdk = typescript_sdk_path },
+    --   },
+    --   filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+    -- },
+    -- html = { filetypes = { 'html', 'twig', 'hbs'} },
 
-  lua_ls = {
-    Lua = {
-      workspace = { checkThirdParty = false },
-      telemetry = { enable = false },
-      diagnostics = { globals = { 'vim' } },
-      -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-      -- diagnostics = { disable = { 'missing-fields' } },
+    lua_ls = {
+        Lua = {
+            workspace = { checkThirdParty = false },
+            telemetry = { enable = false },
+            diagnostics = { globals = { 'vim' } },
+            -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+            -- diagnostics = { disable = { 'missing-fields' } },
+        },
     },
-  },
 }
 
 -- Setup neovim lua configuration
@@ -177,25 +186,25 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 require('mason-lspconfig').setup {
-  ensure_installed = vim.tbl_keys(servers),
-  automatic_installation = false,
-  automatic_enable = false,
-  -- automatic_enable = {
-  --   exclude = {
-  --     'rust_analyzer',
-  --   },
-  -- },
+    ensure_installed = vim.tbl_keys(servers),
+    automatic_installation = false,
+    automatic_enable = false,
+    -- automatic_enable = {
+    --   exclude = {
+    --     'rust_analyzer',
+    --   },
+    -- },
 }
 
 -- Set up individual LSP servers using new vim.lsp.config API
 for server_name, server_config in pairs(servers) do
-  vim.lsp.config(server_name, {
-    capabilities = capabilities,
-    on_attach = on_attach,
-    settings = server_config,
-    filetypes = server_config.filetypes,
-    init_options = server_config.init_options,
-  })
+    vim.lsp.config(server_name, {
+        capabilities = capabilities,
+        on_attach = on_attach,
+        settings = server_config,
+        filetypes = server_config.filetypes,
+        init_options = server_config.init_options,
+    })
 end
 
 -- Enable all configured LSP servers
