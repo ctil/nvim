21a6880bc46df51406714891a7de3b9386e77aea
ctil <ctil@users.noreply.github.com>
2026-07-17 11:29:52 -0600

    Fix table unpack deprecation errors

--- lua/lsp-setup.lua
+++ lua/lsp-setup.lua
@@ -95,8 +95,9 @@ local vue_ls_config = {
       end
       local ts_client = clients[1]
 
-      local param = table.unpack(result)
-      local id, command, payload = table.unpack(param)
+      local unpack = table.unpack or unpack
+      local param = unpack(result)
+      local id, command, payload = unpack(param)
       ts_client:exec_cmd({
         title = 'vue_request_forward', -- You can give title anything as it's used to represent a command in the UI, `:h Client:exec_cmd`
         command = 'typescript.tsserverRequest',
